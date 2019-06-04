cc794c8c206853aa93bacd78fbf7815f41403e05
Oleg Polezky <polezky@mail.ru>
2019-06-04 09:40:01 +0600

    Tour of Heroes

--- app-src/src/app/app.component.ts
+++ app-src/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reduce-store-app';
+  title = 'Tour of Heroes';
 }
